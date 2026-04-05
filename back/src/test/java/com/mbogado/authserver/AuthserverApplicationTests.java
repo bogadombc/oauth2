3bf78deaa5527eeaaeb6d78b17644f7858ffc26a
Marcelo Bogado <celombc@gmail.com>
2026-04-05 10:52:38 -0300

    arquivos

---
+++ back/src/test/java/com/mbogado/authserver/AuthserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbogado.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
